3f5d46e6fe6dc820684823db3681d6b265cb9639
jdlom <jdaniel.lomenede@gmail.com>
2021-06-22 12:21:32 +0200

    Initial commit

---
+++ dev/dev_history.R
@@ -0,0 +1,3 @@
+package_name <- "jdlomenede"
+usethis::create_tidy_package(package_name, "JDL")
+dir.create(paste0(package_name, "/dev"))
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(jdlomenede)
+
+test_check("jdlomenede")
